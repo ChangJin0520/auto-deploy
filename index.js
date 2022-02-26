bbe3c7614588f6f76454ee9cd0b2d63d52f4a8e3
jin.chang <jin.chagn@qiyuesuo.com>
2022-02-26 17:30:51 +0800

    feat: 测试服务

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const Koa = require('koa');
 const app = new Koa();
 
 app.use(async ctx => {
-    const response = ctx.response;
-
-    console.log(response);
+    ctx.body = 'Hello World!';
 })
 
 app.listen(3003);
+
+console.log('app in http://47.103.67.54:3003');
