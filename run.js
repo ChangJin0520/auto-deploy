28aae658175bf45983b823351cb65095c5f8a602
jin.chang <jin.chagn@qiyuesuo.com>
2022-02-26 23:22:33 +0800

    feat: 修改稿 shell

--- run.js
+++ run.js
@@ -4,16 +4,16 @@ const runScript = (appName) => {
     shell.exec('whoami');
 
     /** 到主目录下 */
-    shell.exec(`cd /root/${appName}`);
+    shell.exec(`cd /root/${appName} && git pull && node app.js`);
 
     /** 进入 appName 项目中 */
     // shell.exec(`cd ${appName}`);
 
     /** git 拉去代码 */
-    shell.exec('git pull');
+    // shell.exec('git pull');
 
     /** 停止原来的服务 */
-    shell.exec('node app.js')
+    // shell.exec('node app.js')
 
     /** 启用服务 */
 }
