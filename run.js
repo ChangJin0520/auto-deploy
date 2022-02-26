bc187a716352b424a352a2846b5907ec262339ef
jin.chang <jin.chagn@qiyuesuo.com>
2022-02-27 00:26:27 +0800

    feat: 停用原来服务

--- run.js
+++ run.js
@@ -1,6 +1,9 @@
 const shell = require('shelljs');
 
 const runScript = (appName) => {
+    /** 停止原来的服务 */
+    shell.exec(`lsof -l -R | grep node | grep root/${appName} -m 1 | awk '{print $2}' | xargs`)
+    
     /** 到主目录下 */
     shell.exec(`cd /root/${appName} && git pull && node app.js`, { async:true });
 
@@ -9,9 +12,6 @@ const runScript = (appName) => {
 
     /** git 拉去代码 */
     // shell.exec('git pull');
-
-    /** 停止原来的服务 */
-    // shell.exec('node app.js')
 
     /** 启用服务 */
 }
