837bd0890844988015e8fbf812ebf35e83bbb0c2
jin.chang <jin.chagn@qiyuesuo.com>
2022-02-27 15:43:32 +0800

    feat: 更新脚本

--- run.js
+++ run.js
@@ -2,8 +2,8 @@ const shell = require('shelljs');
 
 const runScript = (appName) => {
     /** 停止原来的服务 */
-    shell.exec(`lsof -l -R | grep node | grep root/${appName} -m 1 | awk '{print $2}' | xargs kill -s 9`);
-    
+    shell.exec(`lsof -l -R | grep node | grep root/${appName}$ -m 1 | awk '{print $2}' | xargs kill -s 9`);
+
     /** 更新服务 */
     shell.exec(`cd /root/${appName} && git pull && npm run start`, { async: true });
 }
