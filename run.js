7bc381cda2f68145dd67a3fb248a0a0c190f6ce3
jin.chang <jin.chagn@qiyuesuo.com>
2022-02-27 14:22:22 +0800

    feat: 更新脚本

--- run.js
+++ run.js
@@ -2,18 +2,10 @@ const shell = require('shelljs');
 
 const runScript = (appName) => {
     /** 停止原来的服务 */
-    shell.exec(`lsof -l -R | grep node | grep root/${appName} -m 1 | awk '{print $2}' | xargs kill -s 9`)
+    shell.exec(`lsof -l -R | grep node | grep root/${appName} -m 1 | awk '{print $2}' | xargs kill -s 9`);
     
-    /** 到主目录下 */
-    shell.exec(`cd /root/${appName} && git pull && node app.js`, { async:true });
-
-    /** 进入 appName 项目中 */
-    // shell.exec(`cd ${appName}`);
-
-    /** git 拉去代码 */
-    // shell.exec('git pull');
-
-    /** 启用服务 */
+    /** 更新服务 */
+    shell.exec(`cd /root/${appName} && git pull && npm run start`, { async: true });
 }
 
 module.exports = {
